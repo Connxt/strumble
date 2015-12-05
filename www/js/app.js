b71ac3516f85c09787f9a1ebea9effed1102da7a
Jan Ryan M. Felipe <ryanskiefelipe@gmail.com>
2015-12-05 23:59:12 +0800

    test

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 var app = angular.module("strumble", ["ionic", "ngResource"]);
-alert("asd");
+
 app.run(function ($ionicPlatform) {
 	$ionicPlatform.ready(function () {
 		// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
